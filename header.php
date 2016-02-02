05aeb7ed666f8f628ee393d69f4183e80200c180
Kaliel <maxime@kaliel.fr>
2016-02-02 09:53:44 +0100

    clean code

--- header.php
+++ header.php
@@ -13,24 +13,12 @@
     <a href="<?= get_home_url(); ?>" class="header-logo">
         <img src="<?= get_template_directory_uri(); ?>/img/logo.svg" alt="">
     </a>
-
-    <!-- <nav class="header-menu">
-        <a href="#">équipes</a>
-        <a href="#">le club</a>
-        <a href="#">photos</a>
-        <a href="#">inscription</a>
-        <a href="#">contact</a>
-        <a href="#">partenaires</a>
-    </nav> -->
-
-
-        <?php wp_nav_menu([
-            'menu' => 'principal',
-            'container' => false,
-            'items_wrap' => '<nav class="header-menu">%3$s</nav>',
-            'walker' => new MenuWalker()
-        ]); ?>
-
+    <?php wp_nav_menu([
+        'menu' => 'principal',
+        'container' => false,
+        'items_wrap' => '<nav class="header-menu">%3$s</nav>',
+        'walker' => new MenuWalker()
+    ]); ?>
 
     <nav class="header-contact">
         <a href="#"><i class="material-icons">smartphone</i></a>
