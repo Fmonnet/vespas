533add1c0ada3ee8b454490b9cc082814a5b7542
Kaliel <maxime@kaliel.fr>
2016-02-02 11:19:05 +0100

    modifs header

--- header.php
+++ header.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=yes"/>
     <title>Vespas Handball</title>
     <link rel="stylesheet" href="<?= get_stylesheet_uri(); ?>">
-    <link rel="icon" type="image/png" href="<?= get_template_directory_uri();?>/img/favicon.png">
-
+    <link rel="icon" type="image/png" href="<?= get_template_directory_uri(); ?>/img/favicon.png">
 </head>
 <body>
 
 <header class="header">
-    <a href="<?= get_home_url(); ?>" class="header-logo">
-        <img src="<?= get_template_directory_uri(); ?>/img/logo.svg" alt="">
-    </a>
-    <?php wp_nav_menu([
-        'menu' => 'principal',
-        'container' => false,
-        'items_wrap' => '<nav class="header-menu">%3$s</nav>',
-        'walker' => new MenuWalker()
-    ]); ?>
+    <div class="header-inner">
+        <a href="<?= get_home_url(); ?>" class="header-logo">
+            <img src="<?= get_template_directory_uri(); ?>/img/logo.svg" alt="">
+        </a>
+        <?php wp_nav_menu([
+            'menu' => 'principal',
+            'container' => false,
+            'items_wrap' => '<nav class="header-menu">%3$s</nav>',
+            'walker' => new MenuWalker()
+        ]); ?>
 
-    <nav class="header-contact">
-        <a href="#"><i class="material-icons">smartphone</i></a>
-        <a href="#"><i class="material-icons">email</i></a>
-        <a href="#"><i class="material-icons">place</i></a>
-    </nav>
-
+        <nav class="header-contact">
+            <a href="#"><i class="material-icons">smartphone</i></a>
+            <a href="#"><i class="material-icons">email</i></a>
+            <a href="#"><i class="material-icons">place</i></a>
+        </nav>
+    </div>
 </header>
 
 <main class="main">
--- inc/joueurs.php
+++ inc/joueurs.php
@@ -1,18 +1,17 @@
-<?php 
+<?php
 
 //custom post type : joueurs
 function creation_joueurs()
 {
-
     $args = array(
         'public' => true,
-        'label'  => 'Joueurs',
-        'supports' => ['thumbnail','title','editor','custom-fields']
+        'label' => 'Joueurs',
+        'supports' => ['thumbnail', 'title', 'editor', 'custom-fields']
     );
 
-    register_post_type('joueur', $args );
+    register_post_type('joueur', $args);
 }
 
-add_action( 'init', 'creation_joueurs' );
+add_action('init', 'creation_joueurs');
 
 ?>
